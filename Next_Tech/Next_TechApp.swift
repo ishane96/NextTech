da37d42b8cf3256ea0e5faf5ed13d094eb7723c7
ishane96 <achinthaishane96@gmail.com>
2023-05-10 19:13:25 +0100

    initial commit

---
+++ Next_Tech/Next_TechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_TechApp.swift
+//  Next_Tech
+//
+//  Created by Achintha kahawalage on 2023-05-10.
+//
+
+import SwiftUI
+
+@main
+struct Next_TechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
